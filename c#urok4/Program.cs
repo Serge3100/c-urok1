ea4e0f83a63362194019107f3dd7127b2f659833
Sergey <serge3100@mail.ru>
2022-10-05 16:03:49 +0300

    Урок 4 деление

--- c#urok4/Program.cs
+++ c#urok4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
