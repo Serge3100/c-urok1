22410b3ab31e5dbf47fdde5b31550cb7228aabe7
Sergey <serge3100@mail.ru>
2022-10-06 21:01:57 +0300

    7 задание

--- c#urok5/Program.cs
+++ c#urok5/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+Console.WriteLine("Привет Маша");
+
+}
+else
+{
+Console.Write("Привет Незнакомец  - ");
+Console.WriteLine(username);
+
+
+}
